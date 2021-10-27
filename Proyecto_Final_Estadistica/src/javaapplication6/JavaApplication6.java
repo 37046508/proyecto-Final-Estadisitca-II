ec80314f85457bec774f6588490dcfc3fc29e1ab
37046508 <emeletzm@miumg.edu.gt>
2021-10-27 15:15:37 -0600

    tercer comit

---
+++ Proyecto_Final_Estadistica/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,12 @@
+package javaapplication6;
+
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
